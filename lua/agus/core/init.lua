10d38b34bd8139948fb3a23f77b79ccbe76aab09
agus-ameig <39110433+agus-ameig@users.noreply.github.com>
2024-02-21 22:28:16 -0300

    some autocommands

--- lua/agus/core/init.lua
+++ lua/agus/core/init.lua
@@ -1,2 +1,24 @@
 require("agus.core.set")
 require("agus.core.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local gusmeier_group = augroup("GusmeierGroup", {})
+local yank_group = augroup("HighlightYank", {})
+
+autocmd({"TextYankPost"}, {
+  group = yank_group,
+  pattern = "*",
+  callback = function ()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40
+    })
+  end
+})
+
+autocmd({ "BufWritePre" }, {
+	group = gusmeier_group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
