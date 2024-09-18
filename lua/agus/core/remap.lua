d0fc827f72b81eebba849b2db3b6fec94d56923b
agus-ameig <agus.ameig@gmail.com>
2024-09-18 16:37:19 -0300

    added new remaps

--- lua/agus/core/remap.lua
+++ lua/agus/core/remap.lua
@@ -10,6 +10,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+vim.keymap.set("n", "<leader>P", [["+p]])
+
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
@@ -26,3 +28,18 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+
+vim.keymap.set("n", "<leader>ng", function()
+  local file = vim.fn.expand('%:t')
+  local ext = vim.fn.expand('%:e')
+  local base = file:sub(1, #file - #ext - 1)
+
+  local extensions = {'.ts', '.html', '.scss'}
+  local current_index = vim.fn.index(extensions, '.'...ext)
+  local next_ext = extensions[current_index % #extensions + 1]
+
+  local next_file = base..next_ext
+  vim.cmd('edit '.. next_file)
+end, {norempa = true, silent = true})
+
+
